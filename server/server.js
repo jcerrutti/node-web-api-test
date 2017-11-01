a302f9ada04dbeddc8f4c30e0c20beb970b37310
unknown <jicerrutti@gmail.com>
2017-10-31 21:18:29 -0300

    Add eslint configuration

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
-var { mongoose } = require('./db/mongoose');
+const { mongoose } = require('./db/mongoose');
 
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
-var app = express();
+const app = express();
+
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
+    const todo = new Todo({
         text: req.body.text
     });
 
@@ -22,19 +23,19 @@ app.post('/todos', (req, res) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
-    })
-})
+    });
+});
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
-    })
+    });
 });
 
 app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
+    const { id } = req.params;
     if (!ObjectID.isValid(id)) {
         res.status(404).send();
     }
@@ -53,4 +54,4 @@ app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
 
-module.exports = { app }
+module.exports = { app };
