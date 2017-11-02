d6af455324b23da2d603e730927b87b7d48851b4
Juan Cerrutti <jicerrutti@gmail.com>
2017-11-01 21:01:24 -0300

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -40,11 +40,11 @@ app.get('/todos/:id', (req, res) => {
         res.status(404).send();
     }
     Todo.findById(id).then((todo) => {
-        if (todo) {
+        if (!todo) {
             return res.status(404).send();
         }
 
-        res.send({ todo });
+        return res.send({ todo });
     }).catch((e) => {
         res.status(400).send(e);
     });
@@ -63,8 +63,8 @@ app.delete('/todos/:id', (req, res) => {
         res.send({ todo });
     }).catch((e) => {
         res.status(400).send(e);
-    })
-})
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
